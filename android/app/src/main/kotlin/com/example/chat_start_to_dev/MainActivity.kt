bff798aa96a7731306b1a9f295e1052c708d7c97
Marcos Mantovani <mantovani.marcos@gmail.com>
2023-01-04 23:34:08 -0300

    Basic scaffold for Firebase Firestore connection. Place android/app/google-services.json from firestore config.

---
+++ android/app/src/main/kotlin/com/example/chat_start_to_dev/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chat_start_to_dev
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  runApp(const MyApp());
+
+  await Firebase.initializeApp();
+
+  QuerySnapshot snapshot = await FirebaseFirestore.instance
+      .collection('mensagens')
+      .orderBy('time', descending: false)
+      .get();
+  List docs = snapshot.docs.map((doc) => doc.data()).toList();
+  print(docs);
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
